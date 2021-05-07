8306547764f78d2f0223acde4353e0a4af265c09
Guilherme Lucena <guilherme.lucena@monitoratec.com.br>
2021-05-07 19:17:41 -0300

    first commit

---
+++ examples/kafkademo/src/main/java/kafkademo/EnvioKafka.java
@@ -0,0 +1,29 @@
+package kafkademo;
+
+import java.util.Properties;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class EnvioKafka {
+
+	public static void main(String[] args) {
+		
+		Properties properties = new Properties();
+		properties.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+		properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		try (KafkaProducer<String, String> producer = new KafkaProducer<>(properties)){
+			String topic = "testejava";
+			String value = "mensagem fixa";
+			ProducerRecord<String, String> record = new ProducerRecord<String, String>(topic, value);
+			producer.send(record);
+		}
+		
+		
+	}
+	
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+# Apache Kafka para iniciantes
+
+Conceitos básicos, utilização do Kafka CLI e criação de produtores e consumidores em Java
